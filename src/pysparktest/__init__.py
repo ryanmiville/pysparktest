1585421e5db6034f8d54afd2af4a9b25efa523eb
Ryan Miville <ryan.miville@terminus.com>
2025-08-20 13:04:41 -0400

    Remove unused SparkTestCase and related imports from __init__

--- src/pysparktest/__init__.py
+++ src/pysparktest/__init__.py
@@ -1,17 +1,9 @@
 """PySpark testing utilities with Iceberg support and migrations."""
 
 from .migrations import run_migrations
-from .spark_suite import (
-    SparkTestCase,
-    create_spark_suite_fixture,
-    spark_session,
-    spark_suite,
-)
+from .spark_suite import  spark_session
 
 __all__ = [
     "run_migrations",
-    "SparkTestCase",
-    "create_spark_suite_fixture",
     "spark_session",
-    "spark_suite",
 ]
